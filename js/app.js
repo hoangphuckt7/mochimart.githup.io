a00bf08443a207b1822ab257a4d0d30f72d10e49
Edric <dat.nguyen@agriconnect.com>
2022-05-23 09:37:09 +0700

    Deploy web

---
+++ js/app.js
@@ -0,0 +1,171 @@
+const products = [
+  {
+    id: 1,
+    thumb:
+      "http://mochimart.vn/uploads/plugin/product_items/20/thumb/thumb_350_tonehh.png",
+    title: "Toen xịt dưỡng hoa hồng",
+    url: "http://mochimart.vn/toen-xit-duong-hoa-hong.html",
+    price: "140.000đ",
+    des: "Nước Tắm bé Thảo Dược Dạng Cô Đặc F&P Thảo dược tắm bé F&P đặc biệt an toàn và hiệu quả với trẻ nhỏ Thiên nhiên luôn mang đến những điều nhẹ dịu nhất cho con, do đó, mẹ thường chọn những sản phẩm có chiết xuất tự nhiên để bảo vệ sức khỏe […]",
+  },
+  {
+    id: 2,
+    thumb:
+      "http://mochimart.vn/uploads/plugin/product_items/20/thumb/thumb_350_tonehh.png",
+    title: "Toen xịt dưỡng hoa hồng",
+    url: "http://mochimart.vn/toen-xit-duong-hoa-hong.html",
+    price: "140.000đ",
+    des: "Nước Tắm bé Thảo Dược Dạng Cô Đặc F&P Thảo dược tắm bé F&P đặc biệt an toàn và hiệu quả với trẻ nhỏ Thiên nhiên luôn mang đến những điều nhẹ dịu nhất cho con, do đó, mẹ thường chọn những sản phẩm có chiết xuất tự nhiên để bảo vệ sức khỏe […]",
+  },
+  {
+    id: 3,
+    thumb:
+      "http://mochimart.vn/uploads/plugin/product_items/20/thumb/thumb_350_tonehh.png",
+    title: "Toen xịt dưỡng hoa hồng",
+    url: "http://mochimart.vn/toen-xit-duong-hoa-hong.html",
+    price: "140.000đ",
+    des: "Nước Tắm bé Thảo Dược Dạng Cô Đặc F&P Thảo dược tắm bé F&P đặc biệt an toàn và hiệu quả với trẻ nhỏ Thiên nhiên luôn mang đến những điều nhẹ dịu nhất cho con, do đó, mẹ thường chọn những sản phẩm có chiết xuất tự nhiên để bảo vệ sức khỏe […]",
+  },
+  {
+    id: 4,
+    thumb:
+      "http://mochimart.vn/uploads/plugin/product_items/20/thumb/thumb_350_tonehh.png",
+    title: "Toen xịt dưỡng hoa hồng",
+    url: "http://mochimart.vn/toen-xit-duong-hoa-hong.html",
+    price: "140.000đ",
+    des: "Nước Tắm bé Thảo Dược Dạng Cô Đặc F&P Thảo dược tắm bé F&P đặc biệt an toàn và hiệu quả với trẻ nhỏ Thiên nhiên luôn mang đến những điều nhẹ dịu nhất cho con, do đó, mẹ thường chọn những sản phẩm có chiết xuất tự nhiên để bảo vệ sức khỏe […]",
+  },
+  {
+    id: 5,
+    thumb:
+      "http://mochimart.vn/uploads/plugin/product_items/20/thumb/thumb_350_tonehh.png",
+    title: "Toen xịt dưỡng hoa hồng",
+    url: "http://mochimart.vn/toen-xit-duong-hoa-hong.html",
+    price: "140.000đ",
+    des: "Nước Tắm bé Thảo Dược Dạng Cô Đặc F&P Thảo dược tắm bé F&P đặc biệt an toàn và hiệu quả với trẻ nhỏ Thiên nhiên luôn mang đến những điều nhẹ dịu nhất cho con, do đó, mẹ thường chọn những sản phẩm có chiết xuất tự nhiên để bảo vệ sức khỏe […]",
+  },
+  {
+    id: 6,
+    thumb:
+      "http://mochimart.vn/uploads/plugin/product_items/20/thumb/thumb_350_tonehh.png",
+    title: "Toen xịt dưỡng hoa hồng",
+    url: "http://mochimart.vn/toen-xit-duong-hoa-hong.html",
+    price: "140.000đ",
+    des: "Nước Tắm bé Thảo Dược Dạng Cô Đặc F&P Thảo dược tắm bé F&P đặc biệt an toàn và hiệu quả với trẻ nhỏ Thiên nhiên luôn mang đến những điều nhẹ dịu nhất cho con, do đó, mẹ thường chọn những sản phẩm có chiết xuất tự nhiên để bảo vệ sức khỏe […]",
+  },
+  {
+    id: 7,
+    thumb:
+      "http://mochimart.vn/uploads/plugin/product_items/20/thumb/thumb_350_tonehh.png",
+    title: "Toen xịt dưỡng hoa hồng",
+    url: "http://mochimart.vn/toen-xit-duong-hoa-hong.html",
+    price: "140.000đ",
+    des: "Nước Tắm bé Thảo Dược Dạng Cô Đặc F&P Thảo dược tắm bé F&P đặc biệt an toàn và hiệu quả với trẻ nhỏ Thiên nhiên luôn mang đến những điều nhẹ dịu nhất cho con, do đó, mẹ thường chọn những sản phẩm có chiết xuất tự nhiên để bảo vệ sức khỏe […]",
+  },
+  {
+    id: 8,
+    thumb:
+      "http://mochimart.vn/uploads/plugin/product_items/20/thumb/thumb_350_tonehh.png",
+    title: "Toen xịt dưỡng hoa hồng",
+    url: "http://mochimart.vn/toen-xit-duong-hoa-hong.html",
+    price: "140.000đ",
+    des: "Nước Tắm bé Thảo Dược Dạng Cô Đặc F&P Thảo dược tắm bé F&P đặc biệt an toàn và hiệu quả với trẻ nhỏ Thiên nhiên luôn mang đến những điều nhẹ dịu nhất cho con, do đó, mẹ thường chọn những sản phẩm có chiết xuất tự nhiên để bảo vệ sức khỏe […]",
+  },
+];
+
+var list = document.querySelector("#list");
+var listTV = document.querySelector("#list-tv");
+products.forEach((item) => {
+  list.innerHTML += `
+    <div class="col-md-3 col-sm-6">
+    <div class="product p-2 mb-5">
+      <div class="product_img mb-3">
+        <a href="${item.url}">
+          <img
+            src= "${item.thumb}"
+          />
+        </a>
+      </div>
+      <div class="product_content mb-3 text-center">
+        <div class="product_content-title mb-3">
+          <a href="${item.url}" class="font15">${item.title}</a>
+        </div>
+        <div class="product_content-mota mb-3">
+          <p>
+          ${item.des}
+          </p>
+        </div>
+      </div>
+      <div class="product_price text-center mb-3">
+        <div class="font15">${item.price}</div>
+      </div>
+    </div>
+  </div>
+    `;
+});
+products.forEach((item) => {
+  listTV.innerHTML += `
+    <div class="col-md-3 col-sm-6">
+    <div class="product p-2 mb-5">
+      <div class="product_img mb-3">
+        <a href="${item.url}">
+          <img
+            src= "${item.thumb}"
+          />
+        </a>
+      </div>
+      <div class="product_content mb-3 text-center">
+        <div class="product_content-title mb-3">
+          <a href="${item.url}" class="font15">${item.title}</a>
+        </div>
+        <div class="product_content-mota mb-3">
+          <p>
+          ${item.des}
+          </p>
+        </div>
+      </div>
+      <div class="product_price text-center mb-3">
+        <div class="font15">${item.price}</div>
+      </div>
+    </div>
+  </div>
+    `;
+});
+
+$(document).ready(function () {
+  $("#content-slider").lightSlider({
+    loop: true,
+    keyPress: true,
+    auto: true,
+    constrols: true,
+  });
+  $("#image-gallery").lightSlider({
+    adaptiveHeight: true,
+    gallery: false,
+    item: 1,
+    thumbItem: 9,
+    slideMargin: 0,
+    speed: 4000,
+    auto: true,
+    loop: true,
+    constrols: true,
+    pager: false,
+    onSliderLoad: function () {
+      $("#image-gallery").removeClass("cS-hidden");
+    },
+  });
+});
+
+const card = document.querySelectorAll(".product");
+const arr = [];
+
+for (i = 0; i < card.length; i++) {
+  const height = card[i].clientHeight;
+  arr.push(height);
+}
+console.log(arr);
+
+maxHeight = Math.max(...arr);
+for (i = 0; i < card.length; i++) {
+  card[i].style.height = maxHeight + "px";
+}
